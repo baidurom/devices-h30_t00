7ee05876ee80f92bf455257b473b9c0f523c6f2c
zhangweiping <zhangweiping@baiyi-mobile.com>
2014-02-19 17:42:33 +0800

    change NotificationManagerPlus package name in Phone
    Change-Id: I0b02eff9b5e6be1f008fe2b5c2de3e2bd4d67801

--- custom_app.sh
+++ custom_app.sh
@@ -10,5 +10,11 @@ if [ "$apkBaseName" = "Settings" ];then
         sed -i '/unlock_set_baidu_slide/d' $tempSmaliDir/res/xml/security_settings_picker.xml
     fi
 
+elif [ "$apkBaseName" = "Phone" ];then
+    echo ">>> in custom_app $apkBaseName"
+    echo ">>> use Lcom/mediatek/notification/NotificationPlus instead of Landroid/app/NotificationPlus in Phone"
+    echo ">>> use Lcom/mediatek/notification/NotificationManagerPlus instead of Landroid/app/NotificationManagerPlus in Phone"
+    sed -i 's#Landroid/app/NotificationPlus#Lcom/mediatek/notification/NotificationPlus#g' $tempSmaliDir/smali/com/android/phone/NotificationMgr.smali
+    sed -i 's#Landroid/app/NotificationManagerPlus#Lcom/mediatek/notification/NotificationManagerPlus#g' $tempSmaliDir/smali/com/android/phone/NotificationMgr.smali
 fi
 
