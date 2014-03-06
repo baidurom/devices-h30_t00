4e572b26d51d61bb5cb8cee2e508e7ceffb008c8
zhangweiping <zhangweiping@baiyi-mobile.com>
2014-03-06 12:49:50 +0800

    update mediatek res id in policy, adjust pin lock view layout
    Change-Id: I135d4a8712ddda78662b14e42c9344108b29679a

--- custom_jar.sh
+++ custom_jar.sh
@@ -6,9 +6,11 @@ tempSmaliDir=$2
 if [ "$jarBaseName" = "android.policy" ];then
     echo ">>> in custom_jar $jarBaseName"
     if [ -f $tempSmaliDir/smali/com/android/internal/policy/impl/PhoneWindowManager.smali ];then
-        echo "turn private to public for keyguardIsShowingTq"
+        echo ">>> turn private to public for keyguardIsShowingTq"
         sed -i 's/.method private keyguardIsShowingTq()Z/.method public keyguardIsShowingTq()Z/' $tempSmaliDir/smali/com/android/internal/policy/impl/PhoneWindowManager.smali
 
         sed -i 's/invoke-direct\( .*Lcom\/android\/internal\/policy\/impl\/PhoneWindowManager;->keyguardIsShowingTq()Z\)/invoke-virtual\1/g' $tempSmaliDir/smali/com/android/internal/policy/impl/PhoneWindowManager.smali
     fi
+    echo ">>> update mediatek res id in android.policy"
+    $PORT_ROOT/tools/modifyID.py $PORT_ROOT/devices/h30_t00/overlay/res/merge_update_mediatek_res.txt $tempSmaliDir
 fi
